f42b7ae6cd315dfaaaf04c8382ee046e69369dcf
admin <836400280@qq.com>
2017-06-22 15:57:04 +0800

    6.22修改

--- app/src/main/java/com/dongling/myapplication/Test.java
+++ app/src/main/java/com/dongling/myapplication/Test.java
@@ -2,14 +2,20 @@ package com.dongling.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 /**
  * Created by admin on 2017/6/22.
  */
 
 public class Test extends Activity {
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initView();
+    }
+
+    private void initView() {
     }
 }
