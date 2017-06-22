cf86b259a7f9220a9ea80ce79f6bbe454c626de3
admin <836400280@qq.com>
2017-06-22 15:50:23 +0800

    qq——修改

--- app/src/main/java/com/dongling/myapplication/Test.java
+++ app/src/main/java/com/dongling/myapplication/Test.java
@@ -1,10 +1,15 @@
 package com.dongling.myapplication;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by admin on 2017/6/22.
  */
 
 public class Test extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
